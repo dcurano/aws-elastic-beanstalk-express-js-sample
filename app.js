e0cb2e1ce20586d858147b6d0840f9817ad71afa
dcurano <45924927+dcurano@users.noreply.github.com>
2021-02-14 18:48:16 -0700

    Update app.js
    Corrected for a missing quote

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Saluton mia nomo estas svelta ombra!));
+app.get('/', (req, res) => res.send('Saluton mia nomo estas svelta ombra!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
